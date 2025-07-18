493efb5340aabd61d5cc3ae4a273004208110841
Perviz Necefov <nadjafovp@gmail.com>
2025-07-18 13:58:52 +0400

    positiveOrnegativeNumber

--- src/conditionalStatement/Conditional.java
+++ src/conditionalStatement/Conditional.java
@@ -1,5 +1,25 @@
 package conditionalStatement;
 
 public class Conditional {
+    public static void main(String[] args) {
 
+    }
+
+    //1. Check Positive or Negative Number
+    //
+    //Write a Java program to get a number from the user and print whether it is positive or negative.
+    //
+    //Test Data
+    //Input number: 35
+    //Expected Output :
+    //Number is positive
+    public static void positiveOrnegativeNumber(int n) {
+        if (n > 0){
+            System.out.println("positive");
+        }else if (n < 0){
+            System.out.println("negative");
+        }else {
+            System.out.println("numbers in not - " + n);
+        }
+    }
 }
